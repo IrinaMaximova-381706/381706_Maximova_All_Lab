1f014c73808a3e0a6f163a811fd82b54e5e63e34
IrinaMaximova-381706 <ira.maximova2017@yandex.ru>
2018-12-12 20:10:00 +0300

    Delenie

--- MatrixLib/Matrix.h
+++ MatrixLib/Matrix.h
@@ -16,6 +16,7 @@ public:
   TMatrix operator+(const TMatrix &mt);         // сложение
   TMatrix operator-(const TMatrix &mt);         // вычитание
   TMatrix<T> operator*(const TMatrix<T> &MT);    // умножение
+  TMatrix<T> operator/(const TMatrix<T> &MT);    //деление
 
   template <class ValType2>
   friend istream& operator>>(istream &in, TMatrix<ValType2> &mt);
@@ -37,11 +38,11 @@ TMatrix<T>::TMatrix(const TMatrix<T> &mt) :TVector<TVector<T> >(mt)
 {}//-------------------------------------------------------------------------
 
 template <class T>
-TMatrix<T>::TMatrix(const TVector<TVector<T> > &mt) :TVector<TVector<T> >(mt)
+TMatrix<T>::TMatrix(const TVector<TVector<T> > &mt) : TVector<TVector<T> >(mt)
 {}//-------------------------------------------------------------------------
 
 template <class T>
-TMatrix<T>::~TMatrix() 
+TMatrix<T>::~TMatrix()
 {}//-------------------------------------------------------------------------
 
 template <class T >
@@ -82,18 +83,47 @@ TMatrix<T> TMatrix<T>::operator-(const TMatrix<T> &mt)
 } //-------------------------------------------------------------------------
 
 template <class T>
-TMatrix<T> TMatrix<T>::operator*(const TMatrix<T> &MT)
+TMatrix<T> TMatrix<T>::operator*(const TMatrix<T> &mt)
 {
-  if (this->size != MT.size)
+  if (this->size != mt.size)
     throw MyException("error size operand");
   TMatrix <T> rez(this->size);
   for (int i = 0; i < this->size; i++)
-    for (int j = i; j < this->size; j++) {
+    for (int j = i; j < this->size; j++)
+    {
       for (int k = i; k <= j; k++)
-        rez.vec[i][j - i] += this->vec[i][k - i] * MT.vec[k][j - k];
+        rez.vec[i][j - i] += this->vec[i][k - i] * mt.vec[k][j - k];
     }
   return rez;
 }//-------------------------------------------------------------------------
+
+template <class T>
+TMatrix<T> TMatrix<T>::operator/(const TMatrix<T> &mt)
+{
+  if (this->size != mt.size)
+    throw MyException("error size operand");
+  TMatrix <T> copy(*this);
+  TMatrix <T> rez(this->size);
+  TMatrix <T> copyMt(mt);
+
+  for (int i = 0; i < this->size; i++)
+    rez[i][0] = 1 / copyMt[i][0];
+
+  for (int i = 0; i < this->size - 1; i++)
+    for (int j = 1; j < this->size - i; j++)
+    {
+      if (copyMt[i][j] != 0)
+      {
+        T temp = copyMt[i][j];
+        rez[i][j] = (-1) * copyMt[i][j] * rez[i + j][0];
+        for (int k = j, l = 0; k < this->size - i; k++)
+          copyMt[i][k] = copyMt[i][k] - copyMt[j][l++] * temp;
+      }
+    }
+  rez = copy * rez;
+  return rez;
+}//-------------------------------------------------------------------------
+
 
 template <class ValType2>
 istream& operator>>(istream &in, TMatrix<ValType2> &mt)
@@ -107,6 +137,10 @@ template <class ValType2>
 ostream & operator<<(ostream &out, const TMatrix<ValType2> &mt)
 {
   for (int i = 0; i < mt.size; i++)
+  {
+    for (int k = 0; k < i; k++)
+      out << "  ";
     out << mt.vec[i] << endl;
+  }
   return out;
 }
