9f1b6041f119055c62be58c49313b159b891d746
Irina__Maximova__381706 <36035908+IrinaMaximova-381706@users.noreply.github.com>
2018-12-13 21:41:32 +0300

    Update Matrix_main.cpp

--- Matrix/Matrix_main.cpp
+++ Matrix/Matrix_main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
   cout << "Creating matrix\n\n";
-  TMatrix<int> M1(3), M2(3), R(3), MС(3);
+  TMatrix<int> M1(3), M2(3), R(3), MCin(3);
   int k = 1;
   for (int i = 0; i < 3; i++)
     for (int j = 0; j < 3 - i; j++)
@@ -57,7 +57,7 @@ int main()
   }
 
   cout << "\n\nUsing input / output streams:\n";
-  cin >> MС;
-  cout << "\nMatrix:" << endl << MС;
+  cin >> MCin;
+  cout << "\nMatrix:" << endl << MCin;
   return 0;
 }
